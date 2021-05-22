245e33b42dc85408154fad069af6a257494c7335
Manikant Jha <mjontop0602@gmail.com>
2021-05-22 16:54:23 +0530

    1. Added new Student route 2. Added Update student route

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const Student = require("../models/StudentModel");
 
-router.get("/add", async (req, res) => {
+router.post("/add", async (req, res) => {
   try {
-    console.log("yeah! m in controlles");
+    const new_student = await Student.create(req.body);
+    return res.send({
+      new_student,
+      success: true,
+    });
   } catch (err) {
-    console.log("Error ", err);
+    console.log("Error..... ", err);
+    return res.send({
+      error: err,
+      success: false,
+    });
+  }
+});
+
+router.post("/update/:id", async (req, res) => {
+  try {
+    const updated_student = await Student.update(req.body, {
+      where: { admission_no: req.params.id },
+    });
+    return res.send({
+      updated_student: { ...req.body, admission_no: req.params.id },
+      success: true,
+    });
+  } catch (err) {
+    console.log("Error..... ", err);
+    return res.send({
+      error: err,
+      success: false,
+    });
   }
 });
 
